cbc21e1f81dfd0085e7ab1a2d2b4b64b4ff30166
Giles Thompson <giles@papaiti.com>
2023-10-24 16:33:39 +1300

    refactor(service): drop getWithDefault

--- ember-cli-flash/src/services/flash-messages.js
+++ ember-cli-flash/src/services/flash-messages.js
@@ -10,7 +10,6 @@ import FlashMessage from '../flash/object';
 import objectWithout from '../utils/object-without';
 import { getOwner } from '@ember/application';
 import flashMessageOptions from '../utils/flash-message-options';
-import getWithDefault from '../utils/get-with-default';
 
 export default class FlashMessagesService extends Service {
   @(equal('queue.length', 0).readOnly())
@@ -91,7 +90,7 @@ export default class FlashMessagesService extends Service {
     );
 
     const flashService = this;
-    const allDefaults = getWithDefault(this, 'flashMessageDefaults', {});
+    const allDefaults = this.flashMessageDefaults ?? {};
     const defaults = objectWithout(allDefaults, ['types', 'preventDuplicates']);
 
     const flashMessageOptions = Object.assign({}, defaults, { flashService });
@@ -107,7 +106,7 @@ export default class FlashMessagesService extends Service {
   }
 
   _getOptionOrDefault(key, value) {
-    const defaults = getWithDefault(this, 'flashMessageDefaults', {});
+    const defaults = this.flashMessageDefaults ?? {};
     const defaultOption = get(defaults, key);
 
     if (typeOf(value) === 'undefined') {
@@ -120,12 +119,12 @@ export default class FlashMessagesService extends Service {
   @computed
   get flashMessageDefaults() {
     const config = getOwner(this).resolveRegistration('config:environment');
-    const overrides = getWithDefault(config, 'flashMessageDefaults', {});
+    const overrides = config.flashMessageDefaults ?? {};
     return flashMessageOptions(overrides);
   }
 
   _setDefaults() {
-    const defaults = getWithDefault(this, 'flashMessageDefaults', {});
+    const defaults = this.flashMessageDefaults ?? {};
 
     for (let key in defaults) {
       const classifiedKey = classify(key);
@@ -134,7 +133,7 @@ export default class FlashMessagesService extends Service {
       set(this, defaultKey, defaults[key]);
     }
 
-    this.registerTypes(getWithDefault(this, 'defaultTypes', emberArray()));
+    this.registerTypes(this.defaultTypes ?? emberArray());
   }
 
   _registerType(type) {
